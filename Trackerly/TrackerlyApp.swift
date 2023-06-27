2a1a81b6c038e5c9a079b8ac80ae38e576007cf2
Anna Chan <a.m.chan.1210@gmail.com>
2023-06-27 14:15:04 -0700

    Initial Commit

---
+++ Trackerly/TrackerlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerlyApp.swift
+//  Trackerly
+//
+//  Created by Choon-Weng Chan on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackerlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
