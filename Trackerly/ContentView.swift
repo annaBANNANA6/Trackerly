91d82a6a208faabd367def204258efb6e8c14374
Anna Chan <a.m.chan.1210@gmail.com>
2023-06-28 13:50:24 -0700

    Added mood, centered stuff, changed size

--- Trackerly/ContentView.swift
+++ Trackerly/ContentView.swift
@@ -8,37 +8,92 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var condition : String = ""
     var body: some View {
         NavigationStack{
-            VStack(alignment: .center) {
+            VStack() {
                 NavigationLink(destination: ContentView()){
                     Text("TRACKERLY")
                         .font(.title)
                         .fontWeight(.light)
-                        .foregroundColor(Color(hue: 0.324, saturation: 0.713, brightness: 0.45))
+                        .foregroundColor(Color(hue: 0.322, saturation: 0.658, brightness: 0.561))
                         .multilineTextAlignment(.center)
                 }
                 VStack(alignment: .leading){
                     Text("Hello User")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hue: 0.353, saturation: 0.739, brightness: 0.468))
                     Text("How are you feeling today?")
+                        .foregroundColor(Color(hue: 0.327, saturation: 0.718, brightness: 0.458))
                 }
+                .padding(.horizontal, 40)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                HStack(){
+                    Button(action: {
+                        condition = "That's great! Glad that you are feeling good!"
+                    }) {
+                        Image("happy")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100.0, height: 100.0)
+                    }
+                    Button(action: {
+                        condition = "sfdfadfdsfdsf"
+                    }) {
+                        Image("ok")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100.0, height: 100.0)
+                    }
+                    Button(action: {
+                        condition = "Aw"
+                    }) {
+                        Image("sad")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 120.0, height: 120.0)
+                    }
+                }
+                
 
+                
+                Text(condition)
+                    .font(.caption)
+                    
+                
             }
             Spacer()
-                .frame(height: 600)
+                .frame(height: 400)
             .toolbar{
                 ToolbarItemGroup(placement: .status){
                     NavigationLink(destination: ContentView()){
                         Image("welcome")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100.0, height: 100.0)
+                            
+                            
+                            
                     }
                     NavigationLink(destination: home()){
                         Image("home")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100.0, height: 100.0)
+                            
                     }
                     NavigationLink(destination: calendar()){
                         Image("calendar")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100.0, height: 100.0)
+                            
                     }
                 }
             }
+            .padding(0)
         }
         
     }
